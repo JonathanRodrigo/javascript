d5d99f735e6cabd21102743a1d31484da40c8c39
JONATHAN RODRIGO DE PAULA SOUSA <jonathanmetron@gmail.com>
2021-12-15 12:03:32 -0300

    atualização

---
+++ ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do carro é ${vel}km/h`)
+if(vel > 60){
+    console.log(`Você ultrapassou o limite de velocicdade será multado! velocidade da via e 60 e vc passou a ${ver}`)
+}
+console.log(`Dirija sempre usando cintode seguraça!`)
+
+// CONDIÇÃO SIMPLES!
