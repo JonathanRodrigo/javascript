264c49f1e4132570cf114c9f19628a716b8330b4
JONATHAN RODRIGO DE PAULA SOUSA <jonathanmetron@gmail.com>
2022-02-20 05:12:07 -0300

    atualização

--- aula12ex/EX015/script.js
+++ aula12ex/EX015/script.js
@@ -1,1 +1,11 @@
-alert(`Olá!`)
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano){
+        windows.alert(`[Erro] verifique os dados e tente novamente!`)
+    }else{
+        windows.alert(`tudo ok`)
+    }
+}
